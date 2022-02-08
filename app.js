c7f5bfe732dcfbf2d22d0d78a8ea39a6d9d059b8
pryy <priandycoy@gmail.com>
2022-02-08 21:02:43 +0700

    update melulu

--- app.js
+++ app.js
@@ -5,44 +5,60 @@ const bigTitle = document.querySelector(".big-title");
 const header = document.querySelector("header");
 const translateAbout = document.querySelectorAll(".translateAbout");
 
-
 let header_height = header.offsetHeight;
 
-window.addEventListener('scroll', () => {
-    let scroll = window.pageYOffset;
-    translate.forEach(element => {
-        let speed = element.dataset.speed;
-        element.style.transform = `translateY(${scroll * speed}px)`;
-    })
+window.addEventListener("scroll", () => {
+  let scroll = window.pageYOffset;
+  translate.forEach((element) => {
+    let speed = element.dataset.speed;
+    element.style.transform = `translateY(${scroll * speed}px)`;
+  });
 
-    bigTitle.style.opacity = - scroll / (header_height / 2) + 1;
+  bigTitle.style.opacity = -scroll / (header_height / 2) + 1;
 
-    navbar.classList.toggle('sticky', window.scrollY > 0);
+  navbar.classList.toggle("sticky", window.scrollY > 0);
 
-    if(scroll >= 143) {
-        translateAbout.forEach(element => {
-            let speed = element.dataset.speed;
-            element.style.transform = `translateY(${scroll * speed}px)`;
-        })
-    }
-})
+  if (scroll >= 143) {
+    translateAbout.forEach((element) => {
+      let speed = element.dataset.speed;
+      element.style.transform = `translateY(${scroll * speed}px)`;
+    });
+  }
+});
 
-document.querySelector(".frontend-skill").addEventListener('click', () => {
-    const toggle = document.querySelector(".frontend-skills")
-    toggle.classList.toggle('open-none');
-    
-})
+document.querySelector(".frontend-skill").addEventListener("click", () => {
+  const toggle = document.querySelector(".frontend-skills");
+  toggle.classList.toggle("open-none");
+});
 
-document.querySelector(".uiux-skill").addEventListener('click', () => {
-    const toggle = document.querySelector(".uiux-skills")
-    toggle.classList.toggle('open-none');
-})
+document.querySelector(".uiux-skill").addEventListener("click", () => {
+  const toggle = document.querySelector(".uiux-skills");
+  toggle.classList.toggle("open-none");
+});
 
-document.querySelector(".framework-skill").addEventListener('click', () => {
-    const toggle = document.querySelector(".framework-skills")
-    toggle.classList.toggle('open-none');
-})
+document.querySelector(".framework-skill").addEventListener("click", () => {
+  const toggle = document.querySelector(".framework-skills");
+  toggle.classList.toggle("open-none");
+});
 
 function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
+  menu.classList.toggle("open");
 }
+
+// swiper
+var swiper = new Swiper('.swiper', {
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+
+// swiper
